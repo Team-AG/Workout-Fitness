bb76cbdf467014cc51bb0c7456da166c6ded06f0
ANDREW <66812117+Team-AG@users.noreply.github.com>
2021-02-12 00:27:18 -0500

    Add files via upload

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const Workout = require("../models/workout")
+
+module.exports = function (app) {
+
+    app.post("/api/workouts", function (req, res) {
+        Workout.create({}).then(workoutDb => {
+            res.json(workoutDb);
+
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts", (req, res) => {
+        getAllWorkouts(req, res);
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        getAllWorkouts(req, res);
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.findByIdAndUpdate(
+            {
+                _id: req.params.id,
+            },{
+                $push: {
+                    exercises: req.body
+                }
+            }).then(workoutDb => {
+                res.json(workoutDb);
+        }).catch(err => {
+                res.json(err);
+        });
+    });
+}
+
+function getAllWorkouts(req, res) {
+    Workout.find({}).then(workoutDb => {
+        console.log(workoutDb);
+        res.json(workoutDb);
+
+    }).catch(err => {
+        res.json(err);
+    });
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+  
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
